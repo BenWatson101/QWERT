2d72590c4ea0bba7a197040cf949e6fe32938dc7
zzr202 <97767089+zzr202@users.noreply.github.com>
2024-09-19 13:36:45 -0400

    linear maps should work

--- QWLINEAR_MAP.h
+++ QWLINEAR_MAP.h
@@ -19,8 +19,46 @@ namespace QWERT {
     class LinearHashMap {
         LinkedList<T<A,B>> pairs;
 
+        T<A,B>* temp = nullptr;
+
         void forceAdd(T<A,B>& in) {
             pairs.add(in);
+        }
+
+        bool add(T<A,B>& in) {
+            if(!this->in(in.l)) {
+                pairs.add(in);
+                return true;
+            }
+            return false;
+        }
+
+        bool in(A& find) {
+            for(auto i: pairs) {
+                if(i.l == find) {
+                    temp = &i;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        A& recent() {
+            if(temp != nullptr) {
+                return temp->l;
+            }
+            return A();
+        }
+
+        T<A,B>& recentPair() {
+            if(temp != nullptr) {
+                return *temp;
+            }
+            return T<A,B>();
+        }
+
+        void resetRecent() {
+            temp = nullptr;
         }
     };
 }
